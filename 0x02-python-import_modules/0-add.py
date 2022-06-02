8acc097863a7febea1a7a3e0000ee53f0a12a083
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-02 19:39:55 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a:d} + {b:d} = {result:d}")
+    print(f"{a:d} + {b:d} = {add(a, b):d}")
