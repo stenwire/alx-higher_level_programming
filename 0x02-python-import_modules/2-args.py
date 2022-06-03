d1c082abaa47ba8abf6435a2b1f8f5faaeaac6ac
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-03 01:31:25 +0100

    Fixed script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ =="__main__":
+    from sys import argv
+    list = argv
+    len = len(list) - 1
+    if len == 0:
+            print("{:d} arguements.".format(len))
+    elif len == 1:
+            print("{:d} arguement:".format(len))
+    else:
+        print("{:d} arguements:".format(len))
+    for i, j in enumerate(list):
+        if i > 0:
+            print("{:d}: {:s}".format(i, j))
