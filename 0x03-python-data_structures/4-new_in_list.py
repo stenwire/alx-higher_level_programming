b83a071e4987254750c1fc9c0fcf2668b4f09734
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-05 03:49:15 +0100

    Added script - 5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from hashlib import new
+
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(my_list[i])
+    new_list[idx] = element
+    return new_list
