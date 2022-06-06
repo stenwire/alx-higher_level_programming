e0e9522212e6d37788fc2c4ff7c54af200f31a60
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-06 16:03:30 +0100

    Added script - 8

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(0, 0), tuple_b=(0, 0)):
+    nw_tup = []
+    if len(tuple_a) == 0:
+        return tuple_b
+    if len(tuple_b) == 0:
+        return tuple_a
+    if len(tuple_a) == 1:
+        tuple_a = tuple_a + (0,)
+    if len(tuple_b) == 1:
+        tuple_b = tuple_b + (0,)
+    if len(tuple_a) >= 2:
+        tuple_a = tuple_a[:2]
+    if len(tuple_b) >= 2:
+        tuple_b = tuple_b[:2]
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return (0, 0)
+    for i in range(2):
+        nw_tup.append(tuple_a[i] + tuple_b[i])
+    return tuple(nw_tup)
