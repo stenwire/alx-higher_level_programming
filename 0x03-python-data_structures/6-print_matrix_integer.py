0c39cc8ff6fc075d21d1c8cfb302a0dbdbf5d69e
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-06 15:10:53 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    i = 1
     for mat in matrix:
+        i = 0
         for element in mat:
+            print("{:d}".format(element), end='')
+            i += 1
             if i < len(mat):
                 print(end=' ')
-            print("{:d}".format(element), end='')
-            
         print()
