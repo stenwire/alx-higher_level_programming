341552eb6cf55ff44f91e4aaff624b804f122457
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-08 22:14:42 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = list(map((lambda x: x), my_list))
-    for i in range(0, len(new_list)-1):
-        if new_list[i] == search:
+    for i in range(len(new_list)):
+        if my_list[i] == search:
             new_list.append(replace)
         else:
             new_list.append(my_list[i])
