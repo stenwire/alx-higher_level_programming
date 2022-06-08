fcce36e5eb57f695326068798efc0c237361ffad
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-08 20:18:42 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # new_mat = matrix.copy()
     new_mat = []
     for i in matrix:
         new_mat.append(list(map((lambda x: x*x), i )))   
