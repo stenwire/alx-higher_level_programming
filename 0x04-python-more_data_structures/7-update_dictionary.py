73d9278b56e68472b0ea5f641344ec60a7fc9e2e
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-08 19:58:41 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,4 @@
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
-    else:
-        a_dictionary.update({key:value})
     return(a_dictionary)
