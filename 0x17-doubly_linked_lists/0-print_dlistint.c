543eb5d007e4a7225ce8a7841fa53d7f5034adc6
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-09 22:15:32 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_dlistint - Prints all the elements of a double linked list
- * @h: Head of the linked list
+ * @h: Head of the linked list - sten
  *
  * Return: Number of nodes (size_t)
  */
