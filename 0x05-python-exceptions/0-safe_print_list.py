b8e2ddafb44a42d69db083bd64f305e710e63fe4
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-20 14:46:29 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end="")
-            c += 1
+            j += 1
         except IndexError:
             break
     print()
