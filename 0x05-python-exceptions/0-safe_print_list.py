3db8231a9c2fa66c478666cd55857cad0ce091a2
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-20 14:43:53 +0100

    fixed script - 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    len = 0
-    num_of_elm_to_print = 0
-    new_list = ''
-
-    try:
-        for item in my_list:
-            len += 1
-
-        if x > len:
-            num_of_elm_to_print = len
-        else:
-            num_of_elm_to_print = x
-
-        for i in range(0, num_of_elm_to_print):
-            new_list += str(my_list[i])
-        print (int(new_list))
-        return (num_of_elm_to_print)
-
-    except (NameError, ValueError, SyntaxError):
-        print ('An error occured, pls try again!')
+    elm_to_print = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            elm_to_print += 1
+        except IndexError:
+            break
+    print()
+    return elm_to_print
