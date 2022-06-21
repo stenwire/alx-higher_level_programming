a19ce123416bb2f7a87452a50b9524eedbf52eca
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-22 00:50:50 +0100

    Added script - 3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A square class is defined"""
+
+
+class Square:
+    """A square class that defines a private instance attribute
+    
+    Attributes:
+        __size (int): A private instance attribute.
+    """
+    def __init__(self, size=0):
+        """Initialise the size of the square
+        
+        Args:
+            __size (int): represents the size of a square.
+
+        Returns: None
+        """
+        type_err = 'size must be an integer'
+        val_err = 'size must be >= 0'
+
+        if type(self.__size) != type(10):
+            raise TypeError(type_err)
+        elif self.__size < 0:
+            raise ValueError(val_err)
+        else:
+            self.__size = size
