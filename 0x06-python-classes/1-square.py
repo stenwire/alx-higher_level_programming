c2e21d9b6b534cc18d502680d6cf48a11bb3253d
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-22 00:12:15 +0100

    Added script - 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A square class is defined"""
+
+
+class Square:
+    """A square class that defines a private instance attribute
+    
+    Attributes:
+        attr1 (str): A private instance attribute
+        
+    """
+    def __init__(self, __size):
+        self.__size = __size
