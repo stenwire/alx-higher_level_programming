431a35c9a26bc01b643654b340ad90d8913a49c1
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-27 12:42:42 +0100

    Added script - 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# encoding: utf-8
+"""
+Name of modle: 3-say_my_name
+
+This module contains one function say_my_name()
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints a combination of your first and last name
+
+    Args:
+        first_name, last_name (str): string name
+
+    Returns:
+        Nothing
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+    print('My name is {:s} {:s}'.format(first_name, last_name))
