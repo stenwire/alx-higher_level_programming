b490fccc89a17051364adca0e0f5e21678280010
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-28 02:58:30 +0100

    pep-8 update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,5 +53,6 @@ class Rectangle:
         """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width for j in range(self.__height))
+            string += "\n".join("#" * self.__width \
+                                for j in range(self.__height))
         return string
