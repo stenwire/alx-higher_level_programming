eae488341745b9408be0407cdeb6bbb269441fe9
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-30 00:14:31 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,6 +3,5 @@
 
 
 class LockedClass:
-    """A locked class is a class that only lets 
-    the user dynamically use the preset instance"""
+    """A locked class is a class that only lets the user dynamically use the preset instance"""
     __slots__ = ('first_name')
