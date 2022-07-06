b8ca92b9203e65b8a1df9f7fc887d705c93e9548
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 21:32:04 +0100

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,13 +3,15 @@
 
 
 class Student():
-    """Definition of student class"""
+    """
+    Definition of student class
+    """
+    
     def __init__(self, first_name, last_name, age):
         """Definition of students property"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """Returns dict of obj"""
