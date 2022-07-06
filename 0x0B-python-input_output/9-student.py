22f6f882915ae010c4ac99af61c48510373b9083
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 16:39:46 +0100

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""A class Student that defines some student property"""
-
-
-class Student():
-    """Definition of student clas"""
+class Student:
+    """creates a class Student"""
     def __init__(self, first_name, last_name, age):
-        """Definition of students property"""
+        """initialize instance attributes
+        Args:
+            first_name(str): first name
+            last_name(str): last name
+            age(int): age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-
     def to_json(self):
-        """Method that retrieves a dictionary representation 
-        of a Student instance"""
-        return(self.__dict__)
+        """retrieves a dictionary representation of a Student instance
+        Returns:
+            a dictionary representation of a Student instance
+        """
+        return self.__dict__
