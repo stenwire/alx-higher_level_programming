ebe2e393a9144e9fd1dfe104f74913091e3101ab
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 21:18:38 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-"""
-Contains the write_file function
-"""
+"""A module that write to a file"""
 
 
 def write_file(filename="", text=""):
-    """write to and output a text file"""
-    chars = 1
-    # open file to write
-    with open(filename, mode='w+', encoding='utf-8') as f:
-        f.write(text)
-    # loop through file content
-    with open(filename, encoding='utf-8') as f:
-        content = f.read()
-        for lines in content:
-            chars+=1
-    # output nmba of chars
-    return chars
+    """Write to a file"""
+
+    with open(filename, mode="w", encoding="utf-8") as f_name:
+        size = f_name.write(text)
+    return size
