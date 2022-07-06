490ad357b72a19b995c727aa5936623ef3b65535
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 16:14:15 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@ Contains the apppend_file function
 
 
 def append_write(filename="", text=""):
-    """write to and output a text file"""
+    """"write to and output a text file"""
     # open file to write
     with open(filename, mode='a+', encoding='utf-8') as f:
         chars = f.write(text)
