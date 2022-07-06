2605afcd4d2e2c589be3997dfb18c06a7f33f36f
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 21:24:30 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""
-This module adds all argument from the cmd line to a list,
-and then saves them to a json file
-"""
+"""A script that adds all arguements in to a python list."""
 
-"""docs"""
 from sys import argv
-"""docs"""
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""docs"""
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
-if len(argv) > 1:
-    argv = argv[1:]
+args = argv
+if len(args) > 1:
+    args = args[1:]
 else:
-    argv = []
+    args = []
 try:
-    content = load_from_json_file(filename)
-except:
-    content = []
+    text = load_from_json_file(filename)
+except Exception:
+    text = []
 
 for i in range(1, len(argv)):
     content.append(argv[i])
