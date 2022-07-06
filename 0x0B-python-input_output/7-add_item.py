fcc1da1676c65d219d205a3b592814570592883f
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 21:15:14 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""
+This module adds all argument from the cmd line to a list,
+and then saves them to a json file
+"""
+
+
 from sys import argv
-"""access commandline arguments"""
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-"""create object from JSON file"""
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-"""writes an object to text file, using JSON representation"""
+"""docs"""
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""docs"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""docs"""
+
 
 filename = "add_item.json"
 try:
