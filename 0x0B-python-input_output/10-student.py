0160f0d1399f25415b65ca76cd288a95f028f467
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 21:12:55 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-class Student:
-    """creates a class Student"""
+"""
+A class Student that defines some student property
+"""
+
+
+class Student():
+    """Definition of student clas"""
     def __init__(self, first_name, last_name, age):
-        """initialize instance attributes
-        Args:
-            first_name(str): first name
-            last_name(str): last name
-            age(int): age
-        """
+        """Definition of students property"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance
-        Args:
-            attrs(list): a list of attributes
-        Returns:
-            a dictionary representation of a Student instance
-        """
-        new = {}
-        i = 0
-        count = 0
-        if type(attrs) is list:
-            for key in attrs:
-                if key in self.__dict__:
-                    new[key] = self.__dict__[key]
-            return new
-        return self.__dict__
+        """Method that retrieves a dictionary representation 
+        of a Student instance for given attrs"""
+        if attrs is not None:
+            my_dict = {}
+            for i in attrs:
+                try:
+                    my_dict[i] = self.__dict__[i]
+                except KeyError:
+                    pass
+            return my_dict
+        return(self.__dict__)
