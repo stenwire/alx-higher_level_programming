301856934d1cf4738bc0184d388d9c1eb94b46df
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-07-06 16:37:55 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-This module adds all argument from the cmd line to a list,
-and then saves them to a json file
-"""
-
-
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
+"""access commandline arguments"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""create object from JSON file"""
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""writes an object to text file, using JSON representation"""
 
 filename = "add_item.json"
 try:
     content = load_from_json_file(filename)
 except:
     content = []
+
 for i in range(1, len(argv)):
     content.append(argv[i])
 save_to_json_file(content, filename)
