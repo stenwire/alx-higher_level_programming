f77c4f3845b2dbdf215bb543450b43b80146ad7a
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-06 20:42:39 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,10 +4,10 @@ const process = require('process');
 const args = process.argv;
 const argLength = args.length;
 
-if (argLength <= 2) {
+if (argLength <= 2 || isNaN(Number(args[2]))) {
   console.log('Missing number of occurrences');
-} else if (isNaN(Number(args[2]))) {
-  console.log('Missing number of occurrences');
+}/* else if () {
+gggghhhhhhhhh*/
 } else {
   let count = 0;
   while (count < args[2]) {
