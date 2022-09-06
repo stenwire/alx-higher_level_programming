04679fe7154426c098c43112afab8d6e08454034
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-06 20:29:30 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,19 +3,22 @@
 const process = require('process');
 const args = process.argv;
 const argLength = args.length;
+const num = parseInt(argv[2]);
 
 function myFactorial (a) {
-  if (argLength <= 2) {
-    console.log(1);
+  if (argLength <= 2 || Number.isNaN(num) {
+    return 1;
   } else {
     let answer = 1;
     let i = a;
     while (i >= 1) {
-      answer *= i;
+      answer = myFactorial(i);
       i--;
     }
-    console.log(answer);
+    return answer * i;
   }
 }
 
-myFactorial(args[2]);
+
+
+
