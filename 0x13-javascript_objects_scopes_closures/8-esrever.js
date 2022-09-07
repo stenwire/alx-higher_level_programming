08188608a235f4b49440990b06cbde82523fcdb2
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-07 04:20:26 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+
+  let myList = [];
+
+  for (let i = (list.length - 1); i >= 0; i--) {
+
+    myList.push(list[i]);
+
+  }
+
+  return myList;
+
+}
+
+/*console.log(esrever([1, 2, 3, 4, 5]));*/
