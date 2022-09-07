d317d4fcb198ad08e21b8bdd589263017a7e1d0b
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-06 17:43:53 -0700

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,68 +1,9 @@
 #!/usr/bin/node
-// const Rectangle = require('./5-square');
-
-// +++++++++++++++++++++++++++++++
-
-
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this.Rectangle;
-    } else {
-      this.width = w,
-      this.height = h
-    }
-  }
-  print = function () {
-    let myStr = '';
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        myStr += 'X';
-      }
-      console.log(myStr);
-      myStr = '';
-    }
-  }
-  rotate = function () {
-    let temp = null
-    temp = this.width
-    this.width = this.height
-    this.height = temp
-    this.height = this.height
-  }
-  double = function () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-};
-
-
-class SquareD extends Rectangle {
-  constructor(size, w, h) {
-    super(w, h)
-    this.size = size
-  }
-  print = function () {
-    let myStr = '';
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        myStr += 'X';
-      }
-      console.log(myStr);
-      myStr = '';
-    }
-  }
-  double = function () {
-    this.size = this.size * 2;
-  }
-};
-
-
-
+const Rectangle = require('./5-square');
 
 // +++++++++++++++++++++++++++++++++++
 
-class Square extends SquareD {
+class Square extends Square {
   constructor(size, mySize) {
     super(size)
     this.mySize = mySize
@@ -93,9 +34,3 @@ class Square extends SquareD {
     }
   }
 }
-
-
-const s1 = new Square(4);
-s1.charPrint();
-
-s1.charPrint('C');
