898dab01bb9e14fc7131d66d9f487dade30dfaf2
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-07 04:00:04 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
+/*module.exports = class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
       return this.Rectangle;
@@ -19,4 +19,43 @@ module.exports = class Rectangle {
       myStr = '';
     }
   }
-};
+};*/
+module.exports = class Rectangle {
+
+  constructor(w, h) {
+
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+
+      return this.Rectangle;
+
+    } else {
+
+      this.width = w,
+
+      this.height = h
+
+    }
+
+  }
+
+  print = function () {
+
+    let myStr = '';
+
+    for (let i = 0; i < this.height; i++) {
+
+      for (let j = 0; j < this.width; j++) {
+
+        myStr += 'X';
+
+      }
+
+      console.log(myStr);
+
+      myStr = '';
+
+    }
+
+  }
+  
+ };
