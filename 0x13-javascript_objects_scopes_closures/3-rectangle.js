1e0f6536c67d1aefcb2f98d88f9bcee20a117de5
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-07 03:48:56 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
       return this.Rectangle;
