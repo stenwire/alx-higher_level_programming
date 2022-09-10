405b05df13a8863a23be93e4fba494e060cd16fb
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-10 20:49:09 +0100

    Add MySQLdb task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+MY_HOST = 'localhost'
+MY_USER = argv[1]
+MY_PASS = argv[2]
+MY_DB = argv[3]
+MY_PORT = 3306
+
+db = MySQLdb.connect(
+    host=MY_HOST, user=MY_USER,
+    passwd=MY_PASS, db=MY_DB, port=MY_PORT)
+
+cur = db.cursor()
+cur.execute("select * from states where name like binary 'N%';")
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+MY_HOST = 'localhost'
+MY_USER = argv[1]
+MY_PASS = argv[2]
+MY_DB = argv[3]
+MY_PORT = 3306
+
+db = MySQLdb.connect(
+    host=MY_HOST, user=MY_USER,
+    passwd=MY_PASS, db=MY_DB, port=MY_PORT)
+
+cur = db.cursor()
+cur.execute("select cities.id, cities.name, states.name \
+    FROM cities INNER JOIN states ON cities.state_id=states.id \
+        ORDER BY cities.id;")
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
