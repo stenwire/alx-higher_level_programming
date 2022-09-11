0025b6460f1e156e3a295756234ab8b8c9a71726
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-12 00:55:22 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ def main():
     """
     main app - runs on initialization
     """
-    MY_HOST = 'localhost'
+    MY_HOST = "localhost"
     MY_USER = argv[1]
     MY_PASS = argv[2]
     MY_DB = argv[3]
