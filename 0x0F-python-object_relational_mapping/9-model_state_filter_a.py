6843488255e63dcb2dc595b73d478c950676623f
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-11 23:44:08 +0100

    Add ORM

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+def main():
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    states =  session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+
+import sys
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String
+)
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Schema for states models.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        """returns a string representation of the states"""
+        return f"<State(name={self.name})>"
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'\
+            .format(sys.argv[1], sys.argv[2], \
+                sys.argv[3]), pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
