c759a68309dae4a42e36c4fa5addeaa27ab3f9af
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-23 04:06:41 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  a Bash script that takes in a URL, sends a request to that URL,
-curl -sI $1 | grep Content-Length | cut -d ' ' -f 2
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
