ed3c684f39357d45b00236c62a50a0b8c3bf5745
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-23 04:01:00 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #  a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
-arg=$1
-curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
+curl -s -I "$arg" | grep -i "^Content-Length:" | cut -d' ' -f2
