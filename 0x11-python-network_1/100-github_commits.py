660513ba9e8aef4d96e4ab6adc639e1f92cf7b9b
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-24 06:20:29 +0100

    fix adv task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,19 +6,15 @@ A Python script that fetches https://alx-intranet.hbtn.io/status
 
 if __name__ == "__main__":
     import requests
-    from requests.auth import HTTPBasicAuth
-    import sys, json
+    import sys
 
-    url = 'https://developer.github.com/v3/repos/'
     repo = sys.argv[1]
     owner = sys.argv[2]
-
-    # auth = HTTPBasicAuth()
 
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
     req = requests.get(url)
     req = req.json()
 
-    # print(req.json().get('author'))
-    for x in req:
-        print(x['author'])
+    count = 0
+    for x in req[:10]:
+        print(f"{x['sha']}: {x['author']['login']}")
