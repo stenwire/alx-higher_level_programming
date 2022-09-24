2c575a17abb1a092c29541df643d4597478738b0
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-09-24 05:21:31 +0100

    fix typo

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,19 +4,15 @@ A Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 
-from wsgiref import headers
-
-
 if __name__ == "__main__":
     import requests
     from requests.auth import HTTPBasicAuth
     import sys
 
-    url = 'https://api.github.com/users/'
+    url = 'https://api.github.com/user'
     name = sys.argv[1]
     pwd = sys.argv[2]
-    headers = {'Authorization': 'token %s' %pwd}
-    params = {'username':pwd}
-    print(headers)
-    req = requests.get(f'{url}{name}auth={HTTPBasicAuth(name, pwd)}')
-    print(req.headers.get('id'))
+
+    auth = HTTPBasicAuth(name, pwd)
+    req = requests.get(url, auth=auth)
+    print(req.json().get("id"))
