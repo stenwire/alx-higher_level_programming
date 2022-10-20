02ee1a323593589aeac146da8c55353e4849e1c5
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-10-20 02:43:12 +0100

    fix 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,38 +3,25 @@
 const request = require('request');
 const myArgs = process.argv.slice(2);
 const myURL = `${myArgs[0]}`;
-const key = 'characters';
 
-// request(myURL, function (error, response, body) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     const res = JSON.parse(body).results;
-//     for (let i = 0; i <= res.length; i++) {
-//       for (key in res[i]) {
-//         if (key === 'characters') {
-//           for (let j = 0; j < res[i].characters.length; j++) {
-//             var count = 0;
-//             while (res[i].characters[j] == 'https://swapi-api.hbtn.io/api/people/18/') {
-//               // console.log('found');
-//               j++;
-//               count += 1;
-//             }
-//           }
-//         }
-//       }
-//     }
-//     console.log('count: ', count);
-//   }
-// });
 
-ch_url = 'https://swapi-api.hbtn.io/api/people/18/';
-
-request(ch_url, function (error, response, body) {
+request(myURL, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const res = JSON.parse(body);
-    console.log(res.films.length);
+    const res = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i <= res.length; i++) {
+      for (var key in res[i]) {
+        if (key === 'characters') {
+          for (let j = 0; j < res[i].characters.length; j++) {
+            if (res[i].characters[j].includes('18')) {
+              count += 1;
+            }
+          }
+        }
+      }
+    }
+    console.log(count);
   }
 });
