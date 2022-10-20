0236cee5e36aaf6d65ea81e022f1325f3d174135
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-10-20 02:45:33 +0100

    fix syntax

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,6 @@ const request = require('request');
 const myArgs = process.argv.slice(2);
 const myURL = `${myArgs[0]}`;
 
-
 request(myURL, function (error, response, body) {
   if (error) {
     console.log(error);
@@ -12,7 +11,7 @@ request(myURL, function (error, response, body) {
     const res = JSON.parse(body).results;
     let count = 0;
     for (let i = 0; i <= res.length; i++) {
-      for (var key in res[i]) {
+      for (const key in res[i]) {
         if (key === 'characters') {
           for (let j = 0; j < res[i].characters.length; j++) {
             if (res[i].characters[j].includes('18')) {
